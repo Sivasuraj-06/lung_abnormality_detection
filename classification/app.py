f4e05358895f6097ab091985e63e94222154a431
R Sivasubramaniyan <rajkumarsubramani63@gmail.com>
2025-10-04 00:28:55 +0530

    Update app.py

--- classification/app.py
+++ classification/app.py
@@ -139,7 +139,7 @@ def predict(model, device, image):
 # 7. STREAMLIT WEB APP LAYOUT
 # ------------------------------------------------------------------------------
 st.set_page_config(page_title="Chest X-ray Classifier", page_icon="🩺", layout="wide")
-st.title("Chest X-ray Abnormality Classifier")
+st.title("CliniScan:Lung-Abnormality Classification on Chest X‐rays using EfficientNet Model")
 st.markdown("Upload a chest X-ray image to get a prediction of potential findings.")
 
 # Load model
