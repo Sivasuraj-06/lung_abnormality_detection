ab398ff4622d6f5e816a76786168494e1700f899
R Sivasubramaniyan <rajkumarsubramani63@gmail.com>
2025-10-10 23:51:00 +0530

    Update app.py

--- detection/app.py
+++ detection/app.py
@@ -15,7 +15,7 @@ import pandas as pd
 MODEL_PATH = "best.pt"
 DRIVE_ID = "1Tt7-qfGC8509TGZTMIT_cWIovgVyRiyc"
 MODEL_URL = f"https://drive.google.com/uc?id={DRIVE_ID}"
-LOGO_PATH = "assets/logo.jpg"  # Replace with your actual logo path
+LOGO_PATH = "../assets/logo.jpg"  # Replace with your actual logo path
 
 # ------------------------------------------------------------------------------
 # Page Setup
